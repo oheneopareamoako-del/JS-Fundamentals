9f1f5f47beb0201f3885a53aa1b0d25d26e6bb3f
Lawrencia Ohene Opare Amoako <oheneopareamoako@gmail.com>
2025-09-15 10:39:41 +0000

    Add 2-arguments.js: prints message depending on number of args

---
+++ 2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+if (arg1 === undefined) {
+  console.log("No argument");
+} else if (arg2 === undefined) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
